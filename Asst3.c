d73f4deddd2c98fe30ce1fb8365d8cf2e775a1c1
Daniel Teffenhart <dannyteffenhart@gmail.com>
2020-11-30 18:24:48 -0500

    initial commit

--- Asst3.c
+++ Asst3.c
@@ -1,1 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+int main(int argc, char* argv[]){
+    if(argc < 2) {
+        printf("Provide port number\n");
+        exit(EXIT_FAILURE);
+    }
+    int port = atoi(argv[1]);
+    printf("port:%d\n",port);
+}
--- Makefile
+++ Makefile
@@ -1,1 +1,4 @@
-
+all:
+	gcc Asst3.c -o KKJserver
+clean:
+	rm KKJserver
